b15119e77c2ceaae78b05250c091ea6b439c75ee
Saundie Weiss <saundieweiss@gmail.com>
2015-11-23 17:33:53 -0700

    Started map for generating icons and more

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // alert("script.js is linked!");
 
-//create an AJAX call by plugging in
-
-//Alchemy API key
-
-//Gather the input from the search bar and update the html
+var sectionIconMap = {
+  'Environment': 'fa fa-tree',
+  'Business': 'fa fa-briefcase',
+  'news': 'fa fa-newspaper-o',
+  'Film': 'fa fa-video-camera',
+  'Global': 'fa fa-globe',
+  'Music': 'fa fa-music',
+  'Life and style': 'fa fa-heart'
+}
 
 
 var table = $('.searched');
@@ -15,61 +19,52 @@ $(goButton).on('click', function(event) {
   var userSearch = $('#searchbar').val();
   $('#searchTerm').text("\" " + userSearch + "\" ");
   // console.log(userSearch);
-  // make today's date into the correct format to add to the URL
-
-  var today = new Date();
-  var day = today.getDate();
-  var year = today.getFullYear();
-  var month = (today.getMonth()) + 1;
-  //write a function that will concatate my data
-  var dateArray = [year, month, day, 'T000000'];
-  var dateInput = dateArray.join('');
-  console.log(dateInput);
-
-  // var sectionIcon = $('#icon');
-  // var $linked = $(articlelink);
-  // var $title = $('#Title');
 
   //write a for loop that goes through each article that is called
   //Add the search term  and date to the URL when a user inputs the data
   $.ajax({
-    url: 'http://content.guardianapis.com/search?q=' + userSearch + '&api-key=APIKEY',
+    url: 'http://content.guardianapis.com/search?q=' + userSearch + '&order-by=newest&api-key=APIKEY',
     method: "GET",
     success: function(data) {
       console.log(data);
       for (var i = 0; i < 10; i++) {
-        // for (var j = 0; j < 10; j++) {
 
-          var article = JSON.stringify(data['response']['results'][i]['webTitle']);
-          var url = JSON.stringify(data['response']['results'][i]['webUrl']);
-          var section = JSON.stringify(data['response']['results'][i]['sectionId']);
+        var article = (data['response']['results'][i]['webTitle']);
+        var url = (data['response']['results'][i]['webUrl']);
+        var section = (data['response']['results'][i]['sectionName']);
 
-          var table = $('.searched');
-          var row = $('<tr></tr>');
-          var sectionIcon = $('<td></td>');
-          var cell = $('<td></td>');
-          var cellLink = $('<td><a></a></td>');
-          $(table).append(row);
-          $(row).append(sectionIcon);
-          $(row).append(cell);
-          $(row).append(cellLink);
-          sectionIcon[0].innerText = section;
-          cell[0].innerText = article;
-          cellLink[0].innerText = url;
+        var table = $('.searched');
+        var row = $('<tr></tr>');
+        $(table).append(row);
+
+        var newArray = section.split(" ");
+        var match = false;
+        // console.log(newArray);
+
+        //use a for..in to check all keys in the object and && var match;
+        for (var sectionName in sectionIconMap) {
+          // console.log(sectionIconMap[sectionName]);
+          if (newArray.indexOf(sectionName) !== -1) {
+            var sectionIcon = $('<td class=newsSection><i class=' + "\" " + sectionIconMap[sectionName] + "\" " + '></i>' +"  "+ section + '</td>');
+            $(row).append(sectionIcon);
+          }
         }
-      // }
 
-      //create a loop that iterates through and creates a DOM node for each article
 
-      //append values to DOM
+        //if else is match
 
+        // if (newArray.indexOf('Environment') !== -1) {
+        //   var sectionIcon = $('<td><i class="fa fa-briefcase"></i>' + section + '</td>');
+        //   $(row).append(sectionIcon);
+        // } else {
+        //   var sectionIcon = $('<td><i class="fa fa-bookmark"></i>' + section + '</td>');
+        //   $(row).append(sectionIcon);
+        // }
+        var cell = $('<td class=newsSection><a href= ' + url + ' >' + article + ' </a></td>');
+        $(row).append(cell);
+      }
     }
   });
 });
 
-
-
-// url: 'https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&start=now-1d&end=now&count=10&q.enriched.url.enrichedTitle.keywords.keyword.text=' + userSearch + '&q.enriched.url.publicationDate.date=20151122T000000&return=enriched.url.url,enriched.url.publicationDate,enriched.url.title&apikey=APIKEY',
-
-//           var article = JSON.stringify(data['result']['docs'][i]['source']['enriched']['url']['title']);
-//           var url = JSON.stringify(data['result']['docs'][i]['source']['enriched']['url']['url']);
+//write a map
